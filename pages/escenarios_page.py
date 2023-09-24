a1e8285f9a40b494abce4903b35dcd0f13d96230
galhrrera <juan.galeanoherrera@gmail.com>
2023-09-24 17:40:13 -0500

    se creó la funcionalidad para los gráficos de frecuencia vs potencia en escenarios

--- pages/escenarios_page.py
+++ pages/escenarios_page.py
@@ -10,6 +10,11 @@ import os
 folder_path = 'data/escenarios/escenarios'
 csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
+# Obtener la lista de archivos CSV en la carpeta "data/escenarios/radioaltímetros"
+folder_path_radioaltimetros = 'data/escenarios/radioaltímetros'
+csv_files_radioaltimetros = [f for f in os.listdir(folder_path_radioaltimetros) if f.startswith('radioaltimetro_') and f.endswith('.csv')]
+
+
 # Inicializar la aplicación Dash
 app = dash.Dash(__name__)
 
@@ -35,7 +40,17 @@ def layout():
         ),
         
         # Gráfico de líneas
-        dcc.Graph(id='line-plot-escenario')
+        dcc.Graph(id='line-plot-escenario'),
+
+                # Dropdown para seleccionar el archivo CSV de radioaltímetros
+        dcc.Dropdown(
+            id='csv-dropdown-radioaltimetros',
+            options=[{'label': file, 'value': file} for file in csv_files_radioaltimetros],
+            value=csv_files_radioaltimetros[0]  # Establecer el primer archivo como valor predeterminado
+        ),
+        
+        # Gráfico de líneas para radioaltímetros
+        dcc.Graph(id='line-plot-radioaltimetros')
     ], className="escenarios-container container")
 
 def register_callbacks(app):
@@ -58,6 +73,25 @@ def register_callbacks(app):
         fig.update_layout(title=f'{filename_without_extension}', xaxis_title='Distancia (m)', yaxis_title='Potencia (dBm)')
 
         return fig
+    
+    # Callback para actualizar el gráfico de líneas de radioaltímetros
+    @app.callback(
+        dash.dependencies.Output('line-plot-radioaltimetros', 'figure'),
+        [dash.dependencies.Input('csv-dropdown-radioaltimetros', 'value')]
+    )
+    def update_line_plot_radioaltimetros(selected_csv_radioaltimetros):
+        # Leer los datos desde el archivo CSV seleccionado de radioaltímetros
+        csv_path_radioaltimetros = os.path.join(folder_path_radioaltimetros, selected_csv_radioaltimetros)
+        df_radioaltimetros = pd.read_csv(csv_path_radioaltimetros, delimiter=';')
+        print(df_radioaltimetros)
+        # Obtener el nombre del archivo sin la extensión
+        filename_without_extension_radioaltimetros = os.path.splitext(selected_csv_radioaltimetros)[0]
+
+        # Crear el gráfico de líneas para radioaltímetros
+        fig_radioaltimetros = go.Figure(data=[go.Scatter(x=df_radioaltimetros['frecuencia'], y=df_radioaltimetros['potencia'], mode='lines')])
+        fig_radioaltimetros.update_layout(title=f'{filename_without_extension_radioaltimetros}', xaxis_title='Frecuencia (MHz)', yaxis_title='Potencia (dBm)')
+
+        return fig_radioaltimetros
 
 
 
