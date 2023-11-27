b80470346b48f226e6a5baaf6595ee8929a89c2d
Galhrrera <galhrrera@localhost.localdomain>
2023-11-27 16:22:00 -0500

    Se agregaron los campos de la calculadora

--- pages/calculadora.py
+++ pages/calculadora.py
@@ -78,4 +78,69 @@ def layout():
             dcc.Input(id="cabeceo-maximo-aeronave", type="number", value=0, className="input-field"),
             # Agrega más campos de entrada aquí con etiquetas y IDs correspondientes
         ], className="input-container container"),
+        html.Div([
+            html.Label("Resultado Ángulo de recepción con respecto a la vertical de la Antena RA (Grados)"),
+            html.Div(id="resultado-angulo-recepcion-respecto-vertical-de-antena-RA", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado Ganancia Antena Aeronave (dB)"),
+            html.Div(id="resultado-ganancia-antena-aeronave", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado Insidencia de señal en la superficie Avión"),
+            html.Div(id="resultado-insidencia-señal-superficie-aeronave", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado Distancia Línea de vista"),
+            html.Div(id="resultado-distancia-linea-vista", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado Pérdida por propagación en espacio libre UIT-R P525 (dB)"),
+            html.Div(id="resultado-distancia-linea-vista", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Corrección para un tiempo del 1% IUT-R P525 6dB (dB)"),
+            dcc.Input(id="correcion-para-tiempo", type="number", value=0, className="input-field"),
+            # Agrega más campos de entrada aquí con etiquetas y IDs correspondientes
+        ], className="input-container container"),
+        html.Div([
+            html.Label("Margen Seguridad Femtocelda u Otro (dB)"),
+            dcc.Input(id="margen-seguridad-femtocelda", type="number", value=0, className="input-field"),
+            # Agrega más campos de entrada aquí con etiquetas y IDs correspondientes
+        ], className="input-container container"),
+        html.Div([
+            html.Label("Resultado PIRE (dB)"),
+            html.Div(id="resultado-pire-db", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado PIRE (W)"),
+            html.Div(id="resultado-pire-w", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Ganancia Antena Base Típico 15dBi - 5dBi de till = 10 dB (dB) (dB)"),
+            dcc.Input(id="ganancia-antena-base", type="number", value=0, className="input-field"),
+            # Agrega más campos de entrada aquí con etiquetas y IDs correspondientes
+        ], className="input-container container"),
+        html.Div([
+            html.Label("Pérdida de cable coaxial antena base -3 dB (dB)"),
+            dcc.Input(id="ganancia-antena-base", type="number", value=0, className="input-field"),
+            # Agrega más campos de entrada aquí con etiquetas y IDs correspondientes
+        ], className="input-container container"),
+        html.Div([
+            html.Label("Resultado Tx Base (dBm)"),
+            html.Div(id="resultado-tx-base-dbm", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
+        html.Div([
+            html.Label("Resultado Tx Base (W)"),
+            html.Div(id="resultado-tx-base-w", className="output-field"),
+            # Agrega más campos de salida aquí con etiquetas y IDs correspondientes
+        ], className="output-container container"),
     ], className="calculadora-container container")
